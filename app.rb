b08dde0f1df8850cedc2b7c8d391c897fe3de621
derrick b <dbozich1@gmail.com>
2018-07-23 03:54:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :'views/index'
+  end
+
+  post '/checkout' do
+    binding.pry
+    @session = session
+    @session[:item] = params[:item]
+
+    erb :'views/checkout'
+  end
 end
